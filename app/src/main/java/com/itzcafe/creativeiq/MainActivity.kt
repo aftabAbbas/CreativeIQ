caada7c63cfb63b2e763e8cfab24e0a39f457012
Adnan Ashraf <107193611+adnan0498@users.noreply.github.com>
2023-06-24 19:33:46 +0500

    played the song

--- app/src/main/java/com/itzcafe/creativeiq/MainActivity.kt
+++ app/src/main/java/com/itzcafe/creativeiq/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.itzcafe.creativeiq
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.itzcafe.creativeiq.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var context = this
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        clickListeners()
+    }
+
+    private fun clickListeners() {
+        binding.run {
+            nePlayPause.setOnClickListener {
+                playMusic()
+            }
+        }
+    }
+
+    private fun playMusic() {
+        val afd = assets.openFd("headlight.m4a")
+        val player = MediaPlayer()
+        player.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        player.prepare()
+        player.start()
     }
 }
